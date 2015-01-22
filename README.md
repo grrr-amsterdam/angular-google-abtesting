c7d62bae867a8f9c6b40bd60e4909c9794b228be
Harmen Janssen <harmen@grrr.nl>
2015-01-23 00:18:16 +0100

    Added directive and provider.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# angular-google-abtesting
+# Angular Google A/B Testing
 An implementation of Google A/B testing in AngularJS
---
+++ googleAbTesting.js
@@ -0,0 +1,92 @@
+'use strict';
+(function() {
+	angular.module('googleAbTesting', [])
+	.constant('GOOGLE_EXPERIMENTS', {
+		baseUrl: '//www.google-analytics.com/cx/api.js?experiment=',
+		timeout: 300,
+		maxTries: 8
+	})
+	.provider('googleAbTesting', ['GOOGLE_EXPERIMENTS',
+		  	function GoogleAbTestingProvider(GOOGLE_EXPERIMENTS) {
+		var _experimentId = null,
+			tries = 0,
+			_setExperimentId,
+			_getExperimentId,
+			scriptAdded = false;
+
+		this.setExperimentId = _setExperimentId = function(experimentId) {
+			_experimentId = experimentId;
+		};
+		this.getExperimentId = _getExperimentId = function() {
+			return _experimentId;
+		};
+
+		var _appendScript = function(experimentId) {
+			if (scriptAdded) {
+				return;
+			}
+			scriptAdded = true;
+			var s = document.createElement('script');
+			s.src = GOOGLE_EXPERIMENTS.baseUrl + experimentId;
+			document.body.appendChild(s);
+		};
+
+		var deferred;
+		var _getVariation = function($q, $interval) {
+			deferred = $q.defer();
+
+			if (!this.getExperimentId()) {
+				deferred.reject('Google Experiment ID not set');
+				return deferred.promise;
+			}
+			_appendScript(this.getExperimentId());
+
+			var to = $interval(function loadGoogleApi() {
+				if (typeof cxApi !== 'undefined') {
+					deferred.resolve(cxApi.chooseVariation());
+					$interval.cancel(to);
+					return;
+				}
+				if (++tries > GOOGLE_EXPERIMENTS.maxTries) {
+					deferred.reject('cxApi not loaded within ' +
+						GOOGLE_EXPERIMENTS.maxTries + ' tries');
+					$interval.cancel(to);
+					return;
+				}
+			});
+			return deferred.promise;
+		};
+
+		this.$get = ['$q', '$interval', function($q, $interval) {
+			return {
+				setExperimentId: _setExperimentId,
+				getExperimentId: _getExperimentId,
+				getVariation: function() {
+					return _getVariation.call(this, $q, $interval);
+				}
+			};
+		}];
+	}])
+	.directive('variation', ['$log', 'googleAbTesting', function($log, googleAbTesting) {
+		return {
+			restrict: 'A',
+			link: function(scope, element, attrs) {
+				element.addClass('ng-cloak');
+
+				scope.$watch(attrs.variation, function(value) {
+					googleAbTesting.getVariation().then(function(variation) {
+						if (variation == value) {
+							element.removeClass('ng-cloak');
+							element.removeClass('ng-hide');
+						} else {
+							element.addClass('ng-hide');
+						}
+						return variation;
+					}).catch(function(err) {
+						$log.warn(err);
+					});
+				});
+			}
+		};
+	}]);
+})();
